62088d6fab38ad000b5cce9b0406509c70275e80
Andrei Bas <bas.andrei@gmail.com>
2013-01-01 15:37:11 +0300

    Update Button Example

--- Button/Button.ino
+++ Button/Button.ino
@@ -1,14 +1,27 @@
-byte led = 13;
-byte echoPin = 2;
+int pin = 13;
+int echoPin = 2;
+volatile int state = LOW;
 
-void setup() {                
+void setup()
+{
   Serial.begin(9600);
+  pinMode(pin, OUTPUT);
+  attachInterrupt(0, blink, CHANGE);
 }
 
-void loop() {
-  pulseIn(echoPin,HIGH);
-  digitalWrite(led, HIGH);
-  
-  pulseIn(echoPin,LOW);
-  digitalWrite(led, LOW);
+void loop()
+{
+  digitalWrite(pin, state);
+}
+
+void blink()
+{
+  delay(1);
+  if(digitalRead(echoPin)==HIGH){
+    Serial.println("Real press");
+  }else if(!state){
+    Serial.println("Fake");    
+  }
+  state = !state;
+  delay(10);
 }
